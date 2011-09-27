1cdacc89fcbc233fc3154db6191b314a7431f0d2
Jan Sanchez <janfsd+github@gmail.com>
2011-09-27 00:18:59 -0400

    Renamed README

--- README.mk
+++
@@ -1,6 +0,0 @@
-# MiniTorrent
-
-This is a project a did a couple of years ago to learn Swing. It is a Java bittorrent GUI that uses
-Java Bittorrent API[1] as the library.
-
-[1]http://sourceforge.net/projects/bitext/
